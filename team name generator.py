fe86d8184e31c204b80cfcbe00daf059520f8f97
bantaiboleD <husainmukri2@gmail.com>
2025-05-10 02:26:30 +0530

    Add files via upload

---
+++ team name generator.py
@@ -0,0 +1,6 @@
+
+print("Welcome to the team name generator.")
+bail = input("What's the name of the city you grew up?")
+shabbo = input("what's the name of your favourite animal?")
+print("Your team name could be:" + bail + " " + shabbo + "s")
+print("Hope you like it.")
